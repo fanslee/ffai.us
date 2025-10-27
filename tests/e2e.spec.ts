c4e95b7641f4211bab8114a6885358f7cde45c9e
fanslee <do@fanslee.com>
2025-10-28 00:02:57 +0800

    1
    1

--- tests/e2e.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test('navigate to 我的委托 from home', async ({ page }) => {
-  await page.goto('/')
-  await expect(page.getByText('大熊猫派对 - 首页')).toBeVisible()
-  await page.getByRole('button', { name: '我的委托' }).click()
-  // Taro H5 typically uses hash router
-  await expect(page).toHaveURL(/orders\/index/)
-})
